f11a5fd242d260f2c43e982161ec932bb644d112
mberkayarsln <mberkayarslann@gmail.com>
2026-05-17 16:48:53 +0300

    init

---
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -0,0 +1,23 @@
+package com.pm.patientservice.controller;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+@RequiredArgsConstructor
+public class PatientController {
+    private final PatientService patientService;
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        return ResponseEntity.ok(patientService.getPatients());
+    }
+}
---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,38 @@
+package com.pm.patientservice.service;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PatientService {
+    private final PatientRepository patientRepository;
+
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+        List<PatientResponseDTO> patientResponseDTOS = new ArrayList<>();
+
+        for (Patient patient : patients) {
+            patientResponseDTOS.add(preparePatientResponseDTO(patient));
+        }
+
+        return patientResponseDTOS;
+    }
+
+    private PatientResponseDTO preparePatientResponseDTO(Patient patient) {
+        PatientResponseDTO patientResponseDTO = new PatientResponseDTO();
+        patientResponseDTO.setId(patient.getId().toString());
+        patientResponseDTO.setName(patient.getName());
+        patientResponseDTO.setEmail(patient.getEmail());
+        patientResponseDTO.setAddress(patient.getAddress());
+        patientResponseDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+
+        return patientResponseDTO;
+    }
+}
