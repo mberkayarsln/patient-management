66afd840d165a5f80b8488d9b1eb87da24aa4269
mberkayarsln <mberkayarslann@gmail.com>
2026-05-19 17:10:50 +0300

    create grpc billing service

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,26 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingResponse;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+
+@Slf4j
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver) {
+        log.info("Billing Request Received: {}", billingRequest);
+
+        // business logic
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
